f79be75517c336529a1205d23cbffe69b6b8ef98
csmith120 <taskmaster3654@gmail.com>
2024-10-08 14:20:58 -0500

    project done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ListComponenet } from './list/list.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponenet
+    ListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,18 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponenet {
-  items: string[] = [];
+export class ListComponent implements OnInit {
+  items: { value: string, crossedOut: boolean }[] = [];
   inputValue: string = '';
+  removeValue: string = '';
+
+  ngOnInit() {
+    this.loadItems();
+  }
 
   addItem() {
     if (this.inputValue.trim() !== '') {
-      this.items.push(this.inputValue);
+      this.items.push({ value: this.inputValue, crossedOut: false });
       this.inputValue = '';
+      this.saveItems();
+    }
+  }
+
+  removeItem() {
+    this.items = this.items.filter(item => item.value !== this.removeValue);
+    this.removeValue = '';
+    this.saveItems();
+  }
+
+  toggleItem(index: number) {
+    const item = this.items[index];
+    item.crossedOut = !item.crossedOut;
+    if (item.crossedOut) {
+      this.items.splice(index, 1);
+    }
+    this.saveItems();
+  }
+
+  saveItems() {
+    localStorage.setItem('items', JSON.stringify(this.items));
+  }
+
+  loadItems() {
+    const savedItems = localStorage.getItem('items');
+    if (savedItems) {
+      this.items = JSON.parse(savedItems);
     }
   }
 }
